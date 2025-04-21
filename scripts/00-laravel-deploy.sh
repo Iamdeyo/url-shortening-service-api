29a289176b647cafa09cc20ab86b30b4eb1c53cf
iamdeyo <dtechlord@gmail.com>
2025-04-22 00:11:57 +0100

    back to default

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
-
-echo "Running composer"
-cd /var/www/html
-composer install --no-dev --optimize-autoloader
-
-echo "Caching config..."
-php artisan config:cache
-
-echo "Caching routes..."
-php artisan route:cache
-
-echo "Running migrations..."
-php artisan migrate --force
-
-# Keep the container running
-exec "php-fpm"
+ echo "Running composer"
+ composer global require hirak/prestissimo
+ composer install --no-dev --optimize-autoloader --working-dir=/var/www/html
+ 
+ echo "Caching config..."
+ php artisan config:cache
+ 
+ echo "Caching routes..."
+ php artisan route:cache
+ 
+ echo "Running migrations..."
+ php artisan migrate --force --seed
